38750bfd75c1e686f9924370734037a6203240bf
Dmytro <103310889+Dmytro-Hlushchenko@users.noreply.github.com>
2023-11-30 22:50:40 +0200

    delete-wrong-registrated-from-pages

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,7 @@
 import LoginForm from "components/LoginForm/LoginForm";
-import { selectRegistrated } from "redux/selectors";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
 
 export default function LoginPage() {
-const registrated = useSelector(selectRegistrated)
 
-    if(registrated) return <Navigate to='/contacts' />
     return(
        <LoginForm/>
     )
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,12 +1,6 @@
 import  RegistrationForm  from "components/RegistrationForm/RegistrationForm";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
-import { selectRegistrated } from "redux/selectors";
 
 export default function  RegisterPage() {
-const registrated = useSelector(selectRegistrated)
-
-if(registrated) return <Navigate to='/contacts' />
 
     return (
         <div>
